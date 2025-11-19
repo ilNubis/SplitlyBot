59d4c4d6a754bcc2299d72cc469863db3329db38
Valerio <valerio.panebianco.04@gmail.com>
2025-11-19 18:22:58 +0100

    feat: Base Bot and Proto-Logging implemented

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+from aiogram import Bot, Dispatcher, types, filters
+from dotenv import load_dotenv
+import logging
+import asyncio
+import sys
+import os
+
+load_dotenv()
+
+API_TOKEN: str = os.getenv("BOT_TOKEN")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 
-def main():
-    ...
+# Initialize bot and dispatcher
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+@dp.message(filters.Command("start"))
+async def send_welcome(message: types.Message):
+
+    """
+
+    This handler will be called when user sends `/start` or `/help` command
+
+    """
+
+    await message.reply("Hi!\nI'm SplitBot!")
+
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(dp.start_polling(bot))
