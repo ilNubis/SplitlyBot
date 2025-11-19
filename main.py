13a17972e7b7eed3cdaad15745d00c1fb8bad0d3
Valerio <valerio.panebianco.04@gmail.com>
2025-11-19 18:24:12 +0100

    fix: start command description

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def send_welcome(message: types.Message):
 
     """
 
-    This handler will be called when user sends `/start` or `/help` command
+    This handler will be called when user sends `/start` command
 
     """
 
